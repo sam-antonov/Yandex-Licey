252c1b6c836b94624f41fe1a70475eff6a3e6cc3
unknown <samantonov2005@gmail.com>
2025-12-06 01:54:12 +0300

    Новое название переменной для суммы

--- Финальные задания/1.0.go
+++ Финальные задания/1.0.go
@@ -21,9 +21,9 @@ func main() {
 	beginWorkDate, _ := time.Parse("02.01.2006", dateStr)
 	contractDate := beginWorkDate.AddDate(0, 0, 15)
 
-	sum := n1 + n2 + n3
+	suma := n1 + n2 + n3
 
-	rubles, kops := math.Modf(sum)
+	rubles, kops := math.Modf(suma)
 
 	fmt.Printf("Уважаемый, %s %s %s, доводим до вашего сведения, что бухгалтерия сформировала документы по факту выполненной вами работы.\n", surname, name, lastname)
 	fmt.Printf("Дата подписания договора: %v. Просим вас подойти в офис в любое удобное для вас время в этот день.\n", contractDate.Format("02.01.2006"))
